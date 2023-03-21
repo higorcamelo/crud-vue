d10d49d7f326e9d38b2fc4d3e267c8b1b44a62d0
Higor Camelo <higorcamelo@gmail.com>
2023-03-21 19:44:48 -0300

    Tentando

--- cardapio/src/App.vue
+++ cardapio/src/App.vue
@@ -6,89 +6,44 @@
       <v-btn @click="openAddItemDialog">Adicionar Item</v-btn>
     </v-toolbar>
 
-    <v-data-table :headers="headers" :items="pratos" hide-default-footer>
-      <template v-slot:[`item.actions`]="{ item }">
-        <v-icon @click="editItem(item)">mdi-pencil</v-icon>
-        <v-icon @click="deleteItem(item)">mdi-delete</v-icon>
+    <v-data-table :headers="headers" :items="pratos.data" class="elevation-1" hide-default-footer>
+      <template v-slot:[`pratos.actions`]="{ prato }">
+        <v-icon @click="deleteItem(prato)">mdi-delete</v-icon>
       </template>
     </v-data-table>
 
-    <v-dialog v-model="addItemDialog" max-width="500px">
-      <v-card>
-        <v-card-title>Adicionar Item</v-card-title>
-        <v-card-text>
-          <v-text-field v-model="newItemName" label="Nome"></v-text-field>
-          <v-text-field v-model="newItemPrice" label="Preço"></v-text-field>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn color="primary" @click="addItem">Adicionar</v-btn>
-          <v-btn @click="addItemDialog = false">Cancelar</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
   </div>
 </template>
 <script>
-import axios from 'axios'
+import axios from 'axios';
 export default {
   data () {
     return {
-      items: [],
-      dialog: false,
-      editedIndex: -1,
-      editedItem: {
-        nome_prato: '',
-        preco: null,
-        tipo: ''
-      },
-      defaultItem: {
-        nome_prato: '',
-        preco: null,
-        tipo: ''
-      },
+      pratos: [],
+      erro: null,
       headers: [
         { text: 'Nome', value: 'nome_prato' },
         { text: 'Preço', value: 'preco' },
         { text: 'Tipo', value: 'tipo'},
         { text: 'Actions', value: 'actions', sortable: false }
       ],
-      pratos:[]
     }
   },
-  computed: {
-    formTitle () {
-      return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+  async mounted() {
+    try{
+      const response = await axios.get('http://localhost:1337/api/cardapios')
+      this.pratos = response.data.data
+    } catch(error){
+      this.erro = error
     }
-  },
-  watch: {
-    dialog (val) {
-      val || this.close()
-    }
-  },
-  created () {
-    this.initialize()
   },
   methods: {
-    initialize () {
-      axios.get('http://localhost:1337/api/cardapios')
-        .then(response => {
-          this.items = response.data
-        })
-        .catch(error => {
-          console.log(error)
-        })
-    },
-    editItem (item) {
-      this.editedIndex = this.items.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialog = true
-    },
-    deleteItem (item) {
-      const index = this.items.indexOf(item)
+    deleteItem (prato) {
+      const index = this.pratos.indexOf(prato)
       if (confirm('Are you sure you want to delete this item?')) {
-        axios.delete(`http://localhost:1337/api/cardapios/${item.id}`)
+        axios.delete(`http://localhost:1337/api/cardapios/${prato.id}`)
           .then(() => {
-            this.items.splice(index, 1)
+            this.pratos.splice(index, 1)
           })
           .catch(error => {
             console.log(error)
@@ -103,21 +58,6 @@ export default {
     this.newItemName = ''
     this.newItemPrice = ''
   },
-  addItem () {
-    const newItem = {
-      nome_prato: this.newItemName,
-      preco: this.newItemPrice,
-      tipo: ''
-    }
-    axios.post('http://localhost:1337/api/cardapios', newItem)
-      .then(response => {
-        this.items.push(response.data)
-        this.closeAddItemDialog()
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  },
     close () {
       this.dialog = false
       setTimeout(() => {
@@ -130,7 +70,7 @@ export default {
         // Edit item
         axios.put(`http://localhost:1337/api/cardapios/${this.editedItem.id}`, this.editedItem)
           .then(response => {
-            Object.assign(this.items[this.editedIndex], response.data)
+            Object.assign(this.pratos[this.editedIndex], response.data)
           })
           .catch(error => {
             console.log(error)
@@ -139,7 +79,7 @@ export default {
         // Add new item
         axios.post('http://localhost:1337/api/cardapios', this.editedItem)
           .then(response => {
-            this.items.push(response.data)
+            this.pratos.push(response.data)
           })
           .catch(error => {
             console.log(error)
