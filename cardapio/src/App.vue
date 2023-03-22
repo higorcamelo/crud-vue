86470a743395a6a5ea0cae50b6f2b98fb365b943
Higor Camelo <higorcamelo@gmail.com>
2023-03-22 07:05:39 -0300

    Update App.vue

--- cardapio/src/App.vue
+++ cardapio/src/App.vue
@@ -1,21 +1,19 @@
 <template>
   <div>
-    <v-toolbar>
-      <v-toolbar-title>Itens do Restaurante</v-toolbar-title>
-      <v-spacer></v-spacer>
-    </v-toolbar>
+    <v-app>
+      <h2>Itens do Restaurante</h2>
     <table class="center">
       <tr>
         <td>
-          <v-form @submit.prevent method="criaPrato">
+          <v-form @submit.prevent= 'criaprato' method="criaPrato">
             <td>
-              <v-text-field label="Nome do prato" required class="novoPrato.nome"></v-text-field>
+              <v-text-field label="Nome do prato" required v-model="novoPrato.nome"></v-text-field>
             </td>
             <td>
-              <v-text-field label="Preço do prato" required class="novoPrato.preco"></v-text-field>
+              <v-text-field label="Preço do prato" required v-model="novoPrato.preco"></v-text-field>
             </td>
             <td>
-              <v-select label="Tipo do prato"  required class="novoPrato.tipo" menu-props="auto" :items="['Entrada', 'Carne', 'Massa', 'Bebida', 'Sobremesa']"></v-select>
+              <v-select label="Tipo do prato"  required v-model="novoPrato.tipo" menu-props="auto" :items="['Entrada', 'Carne', 'Massa', 'Bebida', 'Sobremesa']"></v-select>
             </td>
             <td><v-btn color="blue" text type='submit'>Salvar</v-btn></td>
           </v-form>
@@ -35,23 +33,8 @@
         </v-btn>
       </template>
     </v-data-table>
-
-    <v-dialog v-model="editar" max-width="500">
-      <v-card>
-        <v-card-title>
-          <span class="headline">Editar Prato</span>
-        </v-card-title>
-        <v-card-text>
-          <v-form @submit.prevent="atualizaPrato">
-            <v-text-field label="Nome do prato" v-model="novoPrato.nome" required></v-text-field>
-            <v-text-field label="Preço do prato" v-model="novoPrato.preco" required></v-text-field>
-            <v-select label="Tipo do prato" v-model="novoPrato.tipo" :items="tipos" required></v-select>
-            <v-btn type="submit" color="primary">Atualizar</v-btn>
-            <v-btn color="primary" @click="cancelar">Cancelar</v-btn>
-          </v-form>
-        </v-card-text>
-      </v-card>
-    </v-dialog>
+    </v-app>
+    
   </div>
 </template>
 <script>
@@ -93,16 +76,11 @@ export default {
     }
   },
   methods: {
-    async criaPrato(){
+    criaPrato(){
     try {
-      await axios.post('http://localhost:1337/api/cardapios', this.novoPrato);
-      const response = await axios.get('http://localhost:1337/api/cardapios');
-      this.pratos = response.data.data.map(p => {
-        return {
-          id: p.id,
-          ...p.attributes
-        }
-      });
+      axios.post('http://localhost:1337/api/cardapios', this.novoPrato).then(() => {
+        axios.get('http://localhost:1337/api/cardapios')
+      })
     } catch (error) {
       console.log(error);
     }
